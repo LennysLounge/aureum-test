51000d6a807c4055aed5c93c5835370de192b349
Leonard Schüngel <lschuengel@acardo.com>
2026-03-20 13:54:17 +0100

    Remove received file after test is done

--- src/test/java/io/github/lennyslounge/aureum/GoldenMasterTest.java
+++ src/test/java/io/github/lennyslounge/aureum/GoldenMasterTest.java
@@ -3,6 +3,10 @@ package io.github.lennyslounge.aureum;
 import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GoldenMasterTest {
@@ -14,8 +18,12 @@ public class GoldenMasterTest {
 
     @Test
     public void shouldThrow(){
-        assertThatThrownBy(() -> {
-            GoldenMaster.verify("Hello World");
-        }).isInstanceOf(AssertionFailedError.class);
+        assertThatThrownBy(() -> GoldenMaster.verify("Hello World"))
+                .isInstanceOf(AssertionFailedError.class);
+
+        Path receivedFile = Paths.get("src/test/java/io/github/lennyslounge/aureum/GoldenMasterTest.shouldThrow.received.txt");
+        assertThat(receivedFile).exists();
+
+        receivedFile.toFile().delete();
     }
 }
